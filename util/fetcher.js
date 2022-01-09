66df62d360cb8b4792e3510ec67ad7395ff5d881
Prabesh Pathak <prabeshpathak50@gmail.com>
2022-01-09 17:54:36 +0545

    added fetch url for back end server

---
+++ util/fetcher.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL =
+  process.env.NODE_END === "development"
+    ? "http://localhost:8000/api"
+    : `https://${process.env.SITE_NAME}/api`;
+
+const publicFetch = axios.create({
+  baseURL,
+});
+
+export { publicFetch, baseURL };
