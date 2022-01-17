7e7325d169f9d15bd409bf1e0ebf402729a5c912
Prabesh Pathak <prabeshpathak50@gmail.com>
2022-01-17 12:22:00 +0545

    minor changes

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,14 +1,26 @@
-import React,{useContext} from 'react'
-import cn from 'classnames'
+import React, { useContext } from "react";
+import cn from "classnames";
 
-import ModalContext from '../../store/modal'
-import styles from './modal.module.scss'
+import ModalContext from "../../store/modal";
+import { Close } from "../icons";
+import Button from "../button";
+import styles from "./modal.module.scss";
 
-const Modal = ({children, className, ...props})=>{
-    const {ref,setIsVis} = useContext(ModalContext)
-    return (
-        <>
-        
-        </>
-    )
-}
+const Modal = ({ children, className, ...props }) => {
+  const { ref, setIsVis } = useContext(ModalContext);
+  return (
+    <>
+      <div className={cn(styles.modal, className)} {...props}>
+        <div ref={ref} className={styles.modalDialog}>
+          <Button
+            className={styles.closeButton}
+            onClick={() => setIsVis((isOpen) => !isOpen)}
+          >
+            <Close />
+          </Button>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
