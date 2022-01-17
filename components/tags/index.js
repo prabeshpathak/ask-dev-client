67891e4e9fbbb5a0197b1c8fd23f9487cccdbae4
Prabesh Pathak <prabeshpathak50@gmail.com>
2022-01-17 11:31:53 +0545

    completed tags component

--- components/tags/index.js
+++ components/tags/index.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import Link from 'next/link'
-import cn from 'classnames'
+import React from "react";
+import Link from "next/link";
+import cn from "classnames";
 
-const Tag = ({children, className, count, ...props})=>{
-    return count ? (
-        <div>
-            <Link href={{pathnane:'/',query:{tag: children}}}>
-                <a className=''>
-                    {children}
-                </a>
-            </Link>
-            <span>
-                
-            </span>
-        </div>
-    ):(
-        <Link>
-        </Link>
-    )
+import styles from "./tags.module.scss";
 
-    }
+const Tag = ({ children, className, count, ...props }) => {
+  return count ? (
+    <div>
+      <Link href={{ pathnane: "/", query: { tag: children } }}>
+        <a className={cn(styles.tag, className)} {...props}>
+          {children}
+        </a>
+      </Link>
+      <span className={styles.multiplier}>x</span> &nbsp;
+      <span className={styles.count}>{count}</span>
+    </div>
+  ) : (
+    <Link href={{ pathname: "/", query: { tag: children } }}>
+      <a className={cn(styles.tag, className)} {...props}>
+        {children}
+      </a>
+    </Link>
+  );
+};
 
-export default Tag
+export default Tag;
