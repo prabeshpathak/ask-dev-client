1f3f4f54db04ff2650ead95f12cf9522c0adbf81
unknown <prabeshpathak50@gmail.com>
2022-01-31 10:16:08 +0545

    added dev environment

--- util/fetcher.js
+++ util/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'dev'
     ? 'http://localhost:8080/api'
     : `https://${process.env.SITE_NAME}/api`
 
