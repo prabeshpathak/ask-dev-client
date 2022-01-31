3f6b21a3a25f7d4dbf56be81d5610c89fcdee7b1
unknown <prabeshpathak50@gmail.com>
2022-01-31 10:18:21 +0545

    revert back

--- util/fetcher.js
+++ util/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL =
-  process.env.NODE_ENV === 'dev'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
     : `https://${process.env.SITE_NAME}/api`
 
