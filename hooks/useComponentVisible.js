3f21825518ce1f4ae80d27f45ff4730887f5e5e9
unknown <prabeshpathak50@gmail.com>
2022-01-31 10:04:25 +0545

    minor formatting using prettier

--- hooks/useComponentVisible.js
+++ hooks/useComponentVisible.js
@@ -1,15 +1,16 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from "react";
 
 function useComponentVisible(initialIsVisible) {
-  const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible)
-  const ref = useRef(null)
-  const toggleRef = useRef(null)
+  const [isComponentVisible, setIsComponentVisible] =
+    useState(initialIsVisible);
+  const ref = useRef(null);
+  const toggleRef = useRef(null);
 
   const handleHide = (event) => {
-    if (event.key === 'Escape') {
-      setIsComponentVisible(false)
+    if (event.key === "Escape") {
+      setIsComponentVisible(false);
     }
-  }
+  };
 
   const handleClickOutside = (event) => {
     if (
@@ -17,20 +18,20 @@ function useComponentVisible(initialIsVisible) {
       !ref.current.contains(event.target) &&
       !toggleRef?.current?.contains(event.target)
     ) {
-      setIsComponentVisible(false)
+      setIsComponentVisible(false);
     }
-  }
+  };
 
   useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true)
-    document.addEventListener('keydown', handleHide, true)
+    document.addEventListener("click", handleClickOutside, true);
+    document.addEventListener("keydown", handleHide, true);
     return () => {
-      document.removeEventListener('keydown', handleHide, true)
-      document.removeEventListener('click', handleClickOutside, true)
-    }
-  })
+      document.removeEventListener("keydown", handleHide, true);
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
 
-  return { ref, toggleRef, isComponentVisible, setIsComponentVisible }
+  return { ref, toggleRef, isComponentVisible, setIsComponentVisible };
 }
 
-export default useComponentVisible
+export default useComponentVisible;
