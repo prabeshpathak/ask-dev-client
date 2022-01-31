d3f45c65b5e832b2af11b1bd2c6e61c0f04f5246
unknown <prabeshpathak50@gmail.com>
2022-01-31 10:13:26 +0545

    added formatting

--- store/fetch.js
+++ store/fetch.js
@@ -1,50 +1,50 @@
-import React, { createContext, useContext } from 'react'
-import axios from 'axios'
-import { AuthContext } from './auth'
-import { baseURL } from '../util/fetcher'
+import React, { createContext, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "./auth";
+import { baseURL } from "../util/fetcher";
 
-const FetchContext = createContext()
-const { Provider } = FetchContext
+const FetchContext = createContext();
+const { Provider } = FetchContext;
 
 const FetchProvider = ({ children }) => {
-  const { authState } = useContext(AuthContext)
+  const { authState } = useContext(AuthContext);
 
   const authAxios = axios.create({
-    baseURL
-  })
+    baseURL,
+  });
 
   authAxios.interceptors.request.use(
     (config) => {
-      config.headers.Authorization = `Bearer ${authState.token}`
-      return config
+      config.headers.Authorization = `Bearer ${authState.token}`;
+      return config;
     },
     (error) => {
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
-  )
+  );
 
   authAxios.interceptors.response.use(
     (response) => {
-      return response
+      return response;
     },
     (error) => {
-      const code = error && error.response ? error.response.status : 0
+      const code = error && error.response ? error.response.status : 0;
       if (code === 401 || code === 403) {
-        console.log('error code', code)
+        console.log("error code", code);
       }
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
-  )
+  );
 
   return (
     <Provider
       value={{
-        authAxios
+        authAxios,
       }}
     >
       {children}
     </Provider>
-  )
-}
+  );
+};
 
-export { FetchContext, FetchProvider }
+export { FetchContext, FetchProvider };
--- store/tag.js
+++ store/tag.js
@@ -1,23 +1,23 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState, useEffect } from "react";
 
-import { publicFetch } from '../util/fetcher'
+import { publicFetch } from "../util/fetcher";
 
-const TagContext = createContext()
-const { Provider } = TagContext
+const TagContext = createContext();
+const { Provider } = TagContext;
 
 const TagProvider = ({ children }) => {
-  const [tagState, setTagState] = useState(null)
+  const [tagState, setTagState] = useState(null);
 
   useEffect(() => {
     const fetchPopularTags = async () => {
-      const { data } = await publicFetch.get('/tags/populertags')
-      setTagState(data)
-    }
+      const { data } = await publicFetch.get("/tags/populertags");
+      setTagState(data);
+    };
 
-    fetchPopularTags()
-  }, [])
+    fetchPopularTags();
+  }, []);
 
-  return <Provider value={{ tagState }}>{children}</Provider>
-}
+  return <Provider value={{ tagState }}>{children}</Provider>;
+};
 
-export { TagContext, TagProvider }
+export { TagContext, TagProvider };
