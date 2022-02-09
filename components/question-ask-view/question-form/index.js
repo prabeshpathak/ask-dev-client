1eab477c8c4a0f3513a112ab1d5ee7bfd6c5e709
unknown <prabeshpathak50@gmail.com>
2022-02-09 21:53:23 +0545

    adding support for cloudinary

--- components/question-ask-view/question-form/index.js
+++ components/question-ask-view/question-form/index.js
@@ -1,50 +1,75 @@
-import React, { useState, useContext } from 'react'
-import { useRouter } from 'next/router'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
+import React, { useState, useContext } from "react";
+import { useRouter } from "next/router";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-import { FetchContext } from '../../../store/fetch'
+import { FetchContext } from "../../../store/fetch";
 
-import Button from '../../button'
-import Textarea from '../../textarea'
-import FormInput from '../../form-input'
-import TagInput from '../../tag-input'
+import Button from "../../button";
+import Textarea from "../../textarea";
+import FormInput from "../../form-input";
+import TagInput from "../../tag-input";
 
-import styles from './question-form.module.css'
+import styles from "./question-form.module.css";
 
 const QuestionForm = () => {
-  const router = useRouter()
-  const { authAxios } = useContext(FetchContext)
+  const router = useRouter();
+  const { authAxios } = useContext(FetchContext);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [fileInputState, setFileInputState] = useState("");
+  const [previewSource, setPreviewSource] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    previewFile(file);
+    setSelectedFile(file);
+    setFileInputState(e.target.value);
+  };
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    };
+  };
+
+  const uploadImage = (base64EncodedImage)=>{
+    console.log(base64EncodedImage)
+  }
 
   return (
     <Formik
-      initialValues={{ title: '', text: '', tags: [] }}
+      initialValues={{ title: "", text: "", tags: [] }}
       onSubmit={async (values, { setStatus, resetForm }) => {
-        setLoading(true)
+        setLoading(true);
         try {
-          await authAxios.post('questions', values)
-          resetForm({})
-          router.push('/')
+          values.imageEncoded = previewSource;
+          await authAxios.post("questions", values);
+          resetForm({});
+          router.push("/");
         } catch (error) {
-          setStatus(error.response.data.message)
+          setStatus(error.response.data.message);
         }
-        setLoading(false)
+        setLoading(false);
       }}
       validationSchema={Yup.object({
         title: Yup.string()
-          .required('Title is missing.')
-          .max(150, 'Title cannot be longer than 150 characters.')
-          .min(15, 'Title must be at least 15 characters.'),
+          .required("Title is missing.")
+          .max(150, "Title cannot be longer than 150 characters.")
+          .min(15, "Title must be at least 15 characters."),
         text: Yup.string()
-          .required('Body is missing.')
-          .min(30, 'Body must be at least 30 characters.')
-          .max(30000, 'Body cannot be longer than 30000 characters.'),
+          .required("Body is missing.")
+          .min(30, "Body must be at least 30 characters.")
+          .max(30000, "Body cannot be longer than 30000 characters."),
         tags: Yup.array()
-          .required('Please enter at least one tag.')
-          .max(5, 'Please enter no more than 5 tags.')
-          .of(Yup.string().max(15, 'Tag cannot be longer than 15 characters. '))
+          .required("Please enter at least one tag.")
+          .max(5, "Please enter no more than 5 tags.")
+          .of(
+            Yup.string().max(15, "Tag cannot be longer than 15 characters. ")
+          ),
       })}
     >
       {({
@@ -56,7 +81,7 @@ const QuestionForm = () => {
         setFieldValue,
         handleBlur,
         handleSubmit,
-        isSubmitting
+        isSubmitting,
       }) => (
         <form onSubmit={handleSubmit}>
           <div className={styles.container}>
@@ -90,11 +115,28 @@ const QuestionForm = () => {
               type="text"
               name="tags"
               value={values.tags}
-              onChange={(e) => setFieldValue('tags', e, true)}
+              onChange={(e) => setFieldValue("tags", e, true)}
               onBlur={handleBlur}
               hasError={touched.tags && errors.tags}
               errorMessage={errors.tags && errors.tags}
             />
+            <br/>
+            <p>**Selected if any image required</p>
+            <input
+              id="fileInput"
+              type="file"
+              name="image"
+              onChange={handleFileInputChange}
+              value={fileInputState}
+              className="form-input"
+            />
+            {previewSource && (
+                <img
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
           </div>
           <div className={styles.buttonContainer}>
             <p className={styles.status}>{status}</p>
@@ -112,7 +154,7 @@ const QuestionForm = () => {
         </form>
       )}
     </Formik>
-  )
-}
+  );
+};
 
-export default QuestionForm
+export default QuestionForm;
