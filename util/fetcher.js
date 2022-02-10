49277d159600900331a466bd8275544c0a6de8e2
Prabesh Pathak <57650365+prabeshpathak@users.noreply.github.com>
2022-02-10 17:15:49 +0545

    Update fetcher.js

--- util/fetcher.js
+++ util/fetcher.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseURL =
   process.env.NODE_ENV === 'development'
-    ? 'https://ask-dev-me.herokuapp.com//api'
+    ? 'https://ask-dev-me.herokuapp.com/api'
     : `https://ask-dev-me.herokuapp.com/api`
 
 const publicFetch = axios.create({
