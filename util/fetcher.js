b7a7514180acbdc396992e5bce45d0f0743ecb26
notThePrabesh <prabeshpathak50@gmail.com>
2023-05-23 20:31:04 +0545

    fix: env

--- util/fetcher.js
+++ util/fetcher.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/api'
-    : `https://ask-dev-me.herokuapp.com/api`
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/api"
+    : `https://${process.env.SITE_NAME}/api`;
 
 const publicFetch = axios.create({
-  baseURL
-})
+  baseURL,
+});
 
-export { publicFetch, baseURL }
+export { publicFetch, baseURL };
